90db03d460ee942427f64f5030573dbc5d60f23a
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-04-14 11:36:30 -0500

    6 try

--- lap_timer_client.py
+++ lap_timer_client.py
@@ -9,7 +9,8 @@ def main():
     # TODO: Pedir el nombre del archivo al usuario usando input()
     filename = input("Ingrese el nombre del archivo con los tiempos de vuelta: ")
     # TODO: Abrir el archivo y leer el numero de vueltas n
-    
+    f = open(filename)
+    n = int(f.readline())
     # TODO: Crear el cronometro usando lap_timer.init(n)
     
     # TODO: Leer los n tiempos de vuelta y agregarlos con lap_timer.add_lap()
